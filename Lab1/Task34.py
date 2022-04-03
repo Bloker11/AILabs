59a7456d033012ea9a3bdf4c0c383bef8c569a20
Bloker11 <mikiblok80@gmail.com>
2022-04-03 12:25:34 +0200

    Tasks

--- Lab1/Task34.py
+++ Lab1/Task34.py
@@ -8,6 +8,11 @@ print(miasta)
 df = pd.DataFrame({'Rok': [2010], 'Gdansk': [460], 'Poznan': [555], 'Szczecin': [405]})
 
 # df.to_csv('miasta.csv', mode='a', index=False, header=False)
-
-df.set_index('Rok').plot()
+plt.plot(miasta['Rok'], miasta['Gdansk'], color='r', marker='o', label='Gdansk')
+plt.plot(miasta['Rok'], miasta['Poznan'], color='b', marker='o', label='Poznan')
+plt.plot(miasta['Rok'], miasta['Szczecin'], color='y', marker='o', label='Szczecin')
+plt.xlabel('Years')
+plt.ylabel('Popularity')
+plt.title('Popularity in major polish cities')
+plt.legend()
 plt.show()
